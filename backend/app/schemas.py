739bf93cf6130e765c9d18125faebbe1146a6a5c
JVR-07 <machado.jvr07@gmail.com>
2025-06-07 20:46:14 -0700

    Adding schemas

---
+++ backend/app/schemas.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from datetime import datetime
+
+# User
+class UserCreate(BaseModel):
+    email: EmailStr
+
+# Client
+class ClientCreate(BaseModel):
+    name: str
+    alias: str
+    amount_due: float
+    due_day: int
+    email: Optional[str] = None
+    phone: Optional[str] = None
+    user_id: int
+
+class ClientOut(BaseModel):
+    id: int
+    name: str
+    alias: str
+    amount_due: float
+    due_day: int
+    email: Optional[str]
+    phone: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+# Payment
+class PaymentOut(BaseModel):
+    id: int
+    amount: float
+    concept: Optional[str]
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
