ea0a2778562931fc93030171e2b4588c60e6ab98
JVR-07 <machado.jvr07@gmail.com>
2025-06-07 21:06:31 -0700

    Adding get_db funct

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependency que se usa en FastAPI para obtener la sesión DB por request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
