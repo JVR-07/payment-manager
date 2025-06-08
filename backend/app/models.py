4f5cfa15d1e63b90f08caab6dd7b908ea220295b
JVR-07 <machado.jvr07@gmail.com>
2025-06-07 21:40:02 -0700

    Adding user_id in clients table

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String, Numeric, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 from datetime import datetime, timezone
 
 Base = declarative_base()
@@ -10,18 +9,24 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
+    clients = relationship("Client", back_populates="user")
+
 
 class Client(Base):
     __tablename__ = "clients"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     alias = Column(String, nullable=False)
-    amount_due = Column(Numeric(10,2), nullable=False)
+    amount_due = Column(Numeric(10, 2), nullable=False)
     due_day = Column(Integer, nullable=False)
     email = Column(String, nullable=True)
     phone = Column(String, nullable=True)
 
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    user = relationship("User", back_populates="clients")
     payments = relationship("Payment", back_populates="client")
+
 
 class Payment(Base):
     __tablename__ = "payments"
