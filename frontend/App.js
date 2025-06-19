f0cbd71300edc187367d47b836a32559b1d8c7be
JVR-07 <machado.jvr07@gmail.com>
2025-06-19 14:39:54 -0700

    Add UserContext

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './navigation/StackNavigator';
+import { UserProvider } from './components/UserContext';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </UserProvider>
   );
 }
