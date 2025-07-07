b21780a3e93d2edd0a0185acd72bed15e790684e
JVR-07 <machado.jvr07@gmail.com>
2025-07-07 14:44:47 -0700

    Change name of columns

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,28 +6,28 @@ Base = declarative_base()
 
 class Client(Base):
     __tablename__ = "clients"
-    id = Column("ClientID", Integer, primary_key=True, index=True)
-    name = Column("Name", String(80), nullable=False)
-    alias = Column("Alias", String(20), nullable=False)
-    creation_date = Column("CreationDate", Date, nullable=False)
-    email = Column("Email", String(100), nullable=True)
-    phone = Column("Phone", String(20), nullable=True)
+    id = Column("clientid", Integer, primary_key=True, index=True)
+    name = Column("name", String(80), nullable=False)
+    alias = Column("alias", String(20), nullable=False)
+    creation_date = Column("creationdate", Date, nullable=False)
+    email = Column("email", String(100), nullable=True)
+    phone = Column("phone", String(20), nullable=True)
     contracts = relationship("Contract", back_populates="client")
 
 class Contract(Base):
     __tablename__ = "contracts"
-    id = Column("ContractID", Integer, primary_key=True, index=True)
-    first_payment_date = Column("FirstPaymentDate", Date, nullable=False)
-    total_amount = Column("TotalAmount", Numeric(10, 2), nullable=False)
-    total_payments = Column("TotalPayments", SmallInteger, nullable=False)
-    client_id = Column("ClientID", Integer, ForeignKey("clients.ClientID"), nullable=False)
+    id = Column("contractid", Integer, primary_key=True, index=True)
+    first_payment_date = Column("firstpaymentdate", Date, nullable=False)
+    total_amount = Column("totalamount", Numeric(10, 2), nullable=False)
+    total_payments = Column("totalpayments", SmallInteger, nullable=False)
+    client_id = Column("clientid", Integer, ForeignKey("clients.clientid"), nullable=False)
     client = relationship("Client", back_populates="contracts")
     payments = relationship("Payment", back_populates="contract")
 
 class Payment(Base):
     __tablename__ = "payments"
-    id = Column("PaymentID", Integer, primary_key=True, index=True)
-    payment_date = Column("PaymentDate", Date, nullable=False)
-    payment_amount = Column("PaymentAmount", Numeric(10, 2), nullable=False)
-    contract_id = Column("ContractID", Integer, ForeignKey("contracts.ContractID"), nullable=False)
+    id = Column("paymentid", Integer, primary_key=True, index=True)
+    payment_date = Column("paymentdate", Date, nullable=False)
+    payment_amount = Column("paymentamount", Numeric(10, 2), nullable=False)
+    contract_id = Column("contractid", Integer, ForeignKey("contracts.contractid"), nullable=False)
     contract = relationship("Contract", back_populates="payments")
