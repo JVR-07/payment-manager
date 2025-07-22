6fd9507ca89c0de8ec2c16c1e7322b2c8abc79b0
JVR-07 <machado.jvr07@gmail.com>
2025-07-22 16:49:05 -0700

    Add simple hello world

---
+++ frontend/screens/MovementsScreen.js
@@ -0,0 +1,9 @@
+import { View, Text} from 'react-native';
+
+export default function MovementsScreen() {
+    return(
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f2f2f2' }}>
+            <Text style={{ fontSize: 32, fontWeight: 'bold', marginBottom: 40 }}> Hola Payment Manager</Text>
+        </View>    
+    )
+}
