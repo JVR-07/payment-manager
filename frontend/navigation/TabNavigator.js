ed0b3675546d8ddd698aa3f44af00c35cd579f70
JVR-07 <machado.jvr07@gmail.com>
2025-07-22 15:50:54 -0700

    Add MovementsScreen Tab

--- frontend/navigation/TabNavigator.js
+++ frontend/navigation/TabNavigator.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/HomeScreen';
-import UserScreen from '../screens/UserScreen';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import MovementsScreen from "../screens/MovementsScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
-    
-    return (
-        <Tab.Navigator
-        screenOptions={{
-            headerShown: false,
-            tabBarStyle: { backgroundColor: '#f2f2f2' },
-            tabBarActiveTintColor: '#000',
-            tabBarInactiveTintColor: '#888',
-        }}
-        >
-        <Tab.Screen 
-            name="Contracts" 
-            component={HomeScreen}
-            options={{ tabBarLabel: 'Contratos', tabBarIcon: () => null }} 
-        />
-        <Tab.Screen 
-            name="Movements" 
-            component={UserScreen} 
-            options={{ tabBarLabel: 'Movimientos', tabBarIcon: () => null }} 
-        />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { backgroundColor: "#f2f2f2" },
+        tabBarActiveTintColor: "#000",
+        tabBarInactiveTintColor: "#888",
+      }}
+    >
+      <Tab.Screen
+        name="Contracts"
+        component={HomeScreen}
+        options={{ tabBarLabel: "Contratos", tabBarIcon: () => null }}
+      />
+      <Tab.Screen
+        name="Movements"
+        component={MovementsScreen}
+        options={{ tabBarLabel: "Movimientos", tabBarIcon: () => null }}
+      />
+    </Tab.Navigator>
+  );
 }
