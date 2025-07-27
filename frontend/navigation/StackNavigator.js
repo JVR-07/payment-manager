53dde7eddad02d7008068f38fcbd9cb64580408f
JVR-07 <machado.jvr07@gmail.com>
2025-07-27 13:03:44 -0700

    Change initialROuteName to Login

--- frontend/navigation/StackNavigator.js
+++ frontend/navigation/StackNavigator.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../screens/LoginScreen';
-import HomeScreen from '../screens/HomeScreen';
-import TabNavigator from './TabNavigator';
-import AddClientScreen from '../screens/AddClientScreen';
-import DetailsScreen from '../screens/DetailsScreen';
-import WelcomeScreen from '../screens/WelcomeScreen';
-import MovementsScreen from '../screens/MovementsScreen';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import TabNavigator from "./TabNavigator";
+import AddClientScreen from "../screens/AddClientScreen";
+import DetailsScreen from "../screens/DetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Tabs" component={TabNavigator} options={{ headerShown: false }}/>
-      <Stack.Screen name="AddClient" component={AddClientScreen}/>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Tabs"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="AddClient" component={AddClientScreen} />
       <Stack.Screen name="Details" component={DetailsScreen} />
-      <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
