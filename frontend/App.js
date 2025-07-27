51196f91dff3c4f4a7b593669ee41d2a73eecfb0
JVR-07 <machado.jvr07@gmail.com>
2025-07-27 14:45:30 -0700

    Add SafeArea

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import StackNavigator from './navigation/StackNavigator';
-import { UserProvider } from './components/UserContext';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./navigation/StackNavigator";
+import { UserProvider } from "./components/UserContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <UserProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </UserProvider>
+    <SafeAreaProvider>
+      <UserProvider>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </UserProvider>
+    </SafeAreaProvider>
   );
 }
