5cf0d20b746c8595e9a2cdfe23e818a08c5937f7
JVR-07 <machado.jvr07@gmail.com>
2025-07-27 13:04:49 -0700

    Update login desing and add authorizedUsers logic

--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import * as WebBrowser from "expo-web-browser";
-import {
-  makeRedirectUri,
-  useAuthRequest,
-  exchangeCodeAsync,
-} from "expo-auth-session";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
 import { GOOGLE_CLIENT_ID_WEB, GOOGLE_CLIENT_ID_ANDROID } from "@env";
 import { Platform } from "react-native";
 import { BACKEND_LOCALHOST } from "@env";
+
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -20,6 +18,7 @@ const discovery = {
 
 export default function LoginScreen({ navigation }) {
   const [loginError, setLoginError] = useState(false);
+  const [invalidUser, setInvalidUser] = useState(false);
 
   const clientId =
     Platform.OS === "android" ? GOOGLE_CLIENT_ID_ANDROID : GOOGLE_CLIENT_ID_WEB;
@@ -28,7 +27,7 @@ export default function LoginScreen({ navigation }) {
   const [request, response, promptAsync] = useAuthRequest(
     {
       clientId,
-      scopes: ["https://www.googleapis.com/auth/gmail.readonly"],
+      scopes: ["https://www.googleapis.com/auth/gmail.readonly", "email"],
       redirectUri,
       responseType: "code",
     },
@@ -36,6 +35,7 @@ export default function LoginScreen({ navigation }) {
   );
 
   useEffect(() => {
+    setInvalidUser(false);
     const getToken = async () => {
       if (response?.type === "success" && request?.codeVerifier) {
         const { code } = response.params;
@@ -57,17 +57,43 @@ export default function LoginScreen({ navigation }) {
 
           const tokenData = await res.json();
           const accessToken = tokenData.access_token;
+
           if (accessToken) {
-            navigation.navigate("Tabs", {
-              screen: "Movements",
-              params: { accessToken },
-            });
+            try {
+              const userInfoResponse = await fetch(
+                "https://www.googleapis.com/oauth2/v3/userinfo",
+                {
+                  headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                  },
+                }
+              );
+
+              const userInfo = await userInfoResponse.json();
+
+              const authRes = await fetch(
+                `${BACKEND_LOCALHOST}/authorizedusers/`
+              );
+              const authorizedUsers = await authRes.json();
+
+              if (
+                authorizedUsers.find((user) => user.email === userInfo.email)
+              ) {
+                navigation.navigate("Tabs", {
+                  screen: "Home",
+                  params: { accessToken },
+                });
+              } else {
+                setInvalidUser(true);
+                return;
+              }
+            } catch {
+              setLoginError(true);
+            }
           } else {
-            console.log("Error: access_token no recibido.");
             setLoginError(true);
           }
         } catch (error) {
-          console.log(`Error en fetch: ${error}`);
           setLoginError(true);
         }
       }
@@ -76,46 +102,84 @@ export default function LoginScreen({ navigation }) {
     getToken();
   }, [response]);
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("beforeRemove", (e) => {
-        e.preventDefault();
-        navigation.navigate("Tabs", { screen: "Home" });
-    });
-
-    return unsubscribe;
-  }, [navigation]);
-
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f2f2f2",
-      }}
-    >
-      <Text style={{ fontSize: 32, fontWeight: "bold", marginBottom: 40 }}>
-        Payment Manager
-      </Text>
-      {loginError && (
-        <Text style={{ color: "red", marginBottom: 20 }}>
-          Error al iniciar sesión.
-        </Text>
-      )}
-      <TouchableOpacity
-        style={{
-          backgroundColor: "#ffffff",
-          padding: 15,
-          borderRadius: 10,
-          elevation: 3,
-        }}
-        disabled={!request}
-        onPress={() => promptAsync()}
-      >
-        <Text style={{ fontSize: 16, color: "#000" }}>
-          Iniciar sesión con Google
-        </Text>
-      </TouchableOpacity>
+    <View style={styles.bgContainer}>
+      <View style={styles.mainContainer}>
+        <Text style={styles.TittleText}>Payment Manager</Text>
+        <View style={styles.loginButtonContainer}>
+          {loginError && (
+            <Text style={styles.loginError}>Error al iniciar sesión.</Text>
+          )}
+          {invalidUser && (
+            <Text style={styles.loginError}>
+              Usuario sin autorización.
+            </Text>
+          )}
+          <TouchableOpacity
+            style={styles.loginButton}
+            disabled={!request}
+            onPress={() => promptAsync()}
+          >
+            <AntDesign name="google" size={30} color="#fff" />
+            <Text
+              style={{
+                fontSize: 20,
+                color: "#fff",
+                textAlign: "center",
+                verticalAlign: "middle",
+              }}
+            >
+              Iniciar sesión
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  bgContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1A3D63",
+  },
+  mainContainer: {
+    width: "70%",
+    height: "50%",
+    padding: 20,
+    backgroundColor: "#F6FAFD",
+    borderRadius: 25,
+    alignItems: "center",
+  },
+  TittleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 40,
+    textAlign: "center",
+    color: "#1A3D63",
+  },
+  loginButtonContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  loginButton: {
+    backgroundColor: "#1A3D63",
+    width: "100%",
+    padding: 10,
+    borderRadius: 10,
+    elevation: 3,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    verticalAlign: "middle",
+    gap: 10,
+  },
+  loginError: {
+    color: "red",
+    marginBottom: 20,
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
