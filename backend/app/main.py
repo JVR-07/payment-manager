5f63c83268018fc26953cb38b02c74e5e64c7873
JVR-07 <machado.jvr07@gmail.com>
2025-07-27 19:05:26 -0700

    change CORS logic

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,14 +21,9 @@ app = FastAPI()
 
 router = APIRouter()
 
-origins = [
-    "http://localhost:8081",
-    *os.getenv("CORS_ORIGINS", "").split(","), 
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
