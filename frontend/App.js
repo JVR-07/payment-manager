1bc5e8a5b0ac08e4f04a130b1322d00cf35e8278
JVR-07 <machado.jvr07@gmail.com>
2025-08-10 11:59:40 -0700

    Add RootProvider for add all context providers

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./navigation/StackNavigator";
-import { UserProvider } from "./components/UserContext";
+import { RootProvider } from "./components/RootProvider";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <UserProvider>
+      <RootProvider>
         <NavigationContainer>
           <StackNavigator />
         </NavigationContainer>
-      </UserProvider>
+      </RootProvider>
     </SafeAreaProvider>
   );
 }
