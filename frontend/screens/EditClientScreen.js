f150937fca8b1f2f80541599cf7d9bd6133d8e17
JVR-07 <machado.jvr07@gmail.com>
2025-08-13 16:46:57 -0700

    Create EditClientScreen

---
+++ frontend/screens/EditClientScreen.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { BACKEND_URL } from "@env";
+
+export default function AddClientScreen({ navigation, route }) {
+  const { client } = route.params;
+  const [name, setName] = useState(client.name);
+  const [email, setEmail] = useState(client.email);
+  const [phone, setPhone] = useState(client.phone);
+
+  const verifyEmail = (email) => {
+    return email === "" || /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  const handleSave = async () => {
+    if (!name || !phone || !email) {
+      console.log("Nombre, numero y email son obligatorios.");
+      return;
+    }
+    if (!verifyEmail(email)) {
+      console.log("El email no es válido.");
+      return;
+    }
+
+    try {
+      console.log("Logica de edicion pendiente...");
+      navigation.goBack();
+    } catch (error) {
+      console.log("Error de conexión con el servidor: ", error);
+    }
+  };
+
+  return (
+    <ScrollView
+      style={styles.mainView}
+      contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: "center",
+        padding: 20,
+      }}
+    >
+      <View style={styles.bgView}>
+        <Text style={styles.title}>Editar Cliente</Text>
+        <Text>Nombre *</Text>
+        <TextInput
+          style={styles.textInput}
+          value={name}
+          onChangeText={setName}
+          placeholder="Nombre"
+        />
+        <Text>Email *</Text>
+        <TextInput
+          style={styles.textInput}
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Correo electrónico"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Text>Teléfono *</Text>
+        <TextInput
+          style={styles.textInput}
+          value={phone}
+          onChangeText={setPhone}
+          placeholder="Teléfono"
+          keyboardType="phone-pad"
+          maxLength={20}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSave}>
+          {" "}
+          Guardar{" "}
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = {
+  mainView: {
+    backgroundColor: "#1A3D63",
+  },
+  bgView: {
+    backgroundColor: "#F6FAFD",
+    with: "95%",
+    borderRadius: 25,
+    padding: 5,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#1A3D63",
+  },
+  textInput: {
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    width: "100%",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    verticalAlign: "middle",
+    textAlign: "center",
+    color: "#F6FAFD",
+  },
+};
